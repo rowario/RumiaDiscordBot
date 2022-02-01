7596e6b69259c4ef6e5ab1ef6ebf6f7109159d13
Rowario <r0wario@ya.ru>
2022-02-01 10:25:09 +0300

    random streamer: history, clear

--- src/commands/randomStreamer.ts
+++ src/commands/randomStreamer.ts
@@ -1,22 +1,24 @@
-import { Discord, Permission, Slash, SlashOption } from "discordx";
+import { Discord, Permission, Slash, SlashGroup, SlashOption } from "discordx";
 import { CommandInteraction, Role } from "discord.js";
 import { getRepository } from "typeorm";
-import { alreadyChosenEntity } from "../entities/alreadyChosen";
+import { chooseHistoryEntity } from "../entities/chooseHistory";
 import dayjs from "dayjs";
 
 @Discord()
+@SlashGroup("random")
 @Permission(false)
 @Permission({ id: "936403946128412763", type: "ROLE", permission: true })
 class RandomStreamer {
-	@Slash("random_streamer")
-	async random(@SlashOption("role") role: Role, interaction: CommandInteraction) {
-		const chosenRepository = getRepository(alreadyChosenEntity);
+	@Slash("get")
+	async get(@SlashOption("role") role: Role, interaction: CommandInteraction) {
+		const chosenRepository = getRepository(chooseHistoryEntity);
 		const alreadyChosen = (
 			await chosenRepository.find({
 				where: {
 					date: {
 						$gt: dayjs().unix() - 3600,
 					},
+					roleId: role.id,
 				},
 			})
 		).map((x) => x.id);
@@ -28,6 +30,7 @@ class RandomStreamer {
 			.filter((x) => {
 				return alreadyChosen.indexOf(x.id) === -1;
 			});
+
 		const chosen = members.random();
 		if (members.size > 0 && chosen && chosen.presence) {
 			const stream = chosen.presence.activities.filter((x) => x.type === "STREAMING")[0];
@@ -35,11 +38,40 @@ class RandomStreamer {
 			await chosenRepository.save(
 				chosenRepository.create({
 					id: chosen.id,
+					roleId: role.id,
 					date: dayjs().unix(),
 				})
 			);
 
 			await interaction.reply(`Random member: ${chosen.toString()}\nStream link: ${stream.url}`);
 		} else await interaction.reply(`Cannot found any Streamers with this role!`);
+	}
+
+	@Slash("history")
+	async history(interaction: CommandInteraction<"cached">) {
+		const history = await getRepository(chooseHistoryEntity).find({
+			order: {
+				date: 1,
+			},
+		});
+		if (history.length < 1) return void (await interaction.reply("0 items in history list!"));
+		const {
+			guild: { members },
+		} = interaction;
+		await interaction.reply(
+			"Random Streamers history: \n" +
+				history
+					.map(
+						(x, i) =>
+							`**#${i + 1}** - ${members.cache.get(x.id)?.user.toString() ?? `<@${x.id}>`}`
+					)
+					.join("\n")
+		);
+	}
+
+	@Slash("clear")
+	async clear(interaction: CommandInteraction) {
+		await getRepository(chooseHistoryEntity).delete({});
+		await interaction.reply("History successfully cleared!");
 	}
 }
--- src/entities/chooseHistory.ts
+++ src/entities/chooseHistory.ts
@@ -1,12 +1,15 @@
 import { Column, Entity, ObjectID, ObjectIdColumn } from "typeorm";
 
-@Entity("already_chosen")
-export class alreadyChosenEntity {
+@Entity("choose_history")
+export class chooseHistoryEntity {
 	@ObjectIdColumn()
 	_id!: ObjectID;
 
 	@Column()
 	id!: string;
+
+	@Column()
+	roleId!: string;
 
 	@Column()
 	date!: number;
